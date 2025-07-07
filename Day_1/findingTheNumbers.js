7093bec7daa7fb0382f8c0e28b710d318417125c
Reddyprasadmudiveti <reddyprasad2827@gmail.com>
2025-07-07 23:19:50 +0530

    Initiated DSA

--- Day_1/findingTheNumbers.js
+++ Day_1/findingTheNumbers.js
@@ -1,1 +1,26 @@
-// find 
+// find the largest number in an array
+
+const findLargest = (arr) => {
+  let largestNum = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largestNum) {
+      largestNum = arr[i];
+    }
+  }
+  return largestNum;
+};
+
+const arr = [20, 30, 60, 78, 48, 90];
+
+console.log(findLargest(arr))
+
+//90
+
+
+
+//Alternative and Builtin function for find max value in an array
+let largest = Math.max(...arr);
+
+console.log(largest)
+
+//90
